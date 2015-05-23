2caa7807211bd896e7b28c62490193623ce4101a
johan <johan.kallberg88@gmail.com>
2015-05-23 11:56:08 +0200

    android sdk'

---
+++ ShaylaGames-Unity/Assets/Scripts/SafeNumber.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SafeNumber : MonoBehaviour {
+
+	public Transform Dial;
+
+	// Use this for initialization
+	void Start () {
+		if (Dial == null)
+			Debug.LogError ("Set the dial object");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Dial") {
+			Debug.Log ("startTimer");
+			StartCoroutine ("Timer");
+		}
+	}
+
+	//private float startTime = float.MinValue;
+	public float duration = 1.0f;
+	private IEnumerator Performance(){			
+					yield return new WaitForEndOfFrame (); 
+			}
+
+
+
+}
+
+
