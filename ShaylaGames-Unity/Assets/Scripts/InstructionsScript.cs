218917e02a1ad75484abe311c2bfd9916b39d16f
Luke Spierewka <spierek@gmail.com>
2015-05-24 09:00:45 +0200

    [+] Instructions screen

---
+++ ShaylaGames-Unity/Assets/Scripts/InstructionsScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InstructionsScript : MonoBehaviour {
+    #region Variables
+    private bool isVisible = true;
+    private float initialDelay = 5f;
+    private float timer;
+
+    private SpriteRenderer sprite;
+    private Color tempC;
+    #endregion
+
+    #region Monobehaviour Methods
+    private void Awake () {
+        sprite = transform.GetChild(0).GetComponent<SpriteRenderer>();
+    }
+    
+    private void Update () {
+        if (timer > initialDelay && Input.GetMouseButtonDown(0)) {
+            StartCoroutine(Hide());
+        }
+
+        timer += Time.deltaTime;
+    }
+    #endregion
+
+    #region Methods
+    private IEnumerator Hide() {
+        float duration = 2f;
+        float t = duration;
+        tempC = sprite.color;
+
+        while (t > 0) {
+            tempC.a = t / duration;
+            sprite.color = tempC;
+
+            t -= Time.deltaTime;
+            yield return null;
+        }
+
+        sprite.color = new Color(1,1,1,0);
+    }
+    #endregion
+}
