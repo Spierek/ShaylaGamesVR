bfbf171e328ee556446fbf0235116f262e250f80
Luke Spierewka <spierek@gmail.com>
2015-05-24 09:09:31 +0200

    [+] Final bloom lerp

--- ShaylaGames-Unity/Assets/Scripts/PlayerScript.cs
+++ ShaylaGames-Unity/Assets/Scripts/PlayerScript.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.ImageEffects;
 
 public class PlayerScript : MonoBehaviour {
     #region Variables
+    public Bloom bloomLeft;
+    public Bloom bloomRight;
     #endregion
 
     #region Monobehaviour Methods
@@ -14,9 +17,29 @@ public class PlayerScript : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.R)) {
             Application.LoadLevel(0);
         }
+
+        if (Input.GetKeyDown(KeyCode.B)) {
+            StartCoroutine(FinalBloom());
+        }
     }
     #endregion
 
     #region Methods
+    private IEnumerator FinalBloom() {
+        float timer = 0;
+        float duration = 5f;
+        float finalBloom = 15f;
+
+        while (timer < duration) {
+            bloomLeft.bloomIntensity = Mathf.Lerp(0, finalBloom, timer / duration);
+            bloomRight.bloomIntensity = Mathf.Lerp(0, finalBloom, timer / duration);
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        bloomLeft.bloomIntensity = finalBloom;
+        bloomRight.bloomIntensity = finalBloom;
+    }
     #endregion
 }
